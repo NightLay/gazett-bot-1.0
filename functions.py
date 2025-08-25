0da09de430e06f1e2d977481079c1150351c9d5f
NightLay <144587961+NightLay@users.noreply.github.com>
2025-08-25 10:31:21 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,172 @@
+
+from telebot import types
+from config import cnfg, bot, texts, rehost_texts, BOT_LINK, OUR_AD
+
+
+
+def work_menu(user_id):
+
+    if user_id != cnfg.MY_ID:
+        butn_text = '◀️ написать письмо 📬/◀️ моё портфолио 👤/◀️ позвать друга 👥'
+        bot.send_message(user_id, "Я бот помошник редакции \nКаждый читатель здесь на несколько шагов ближе к нам", reply_markup=reply_btn_create(3, butn_text))
+    else:
+        butn_text = '◀️ читатели 👥/◀️ сообщение всем 📬/◀️ код-пароль-ссылка #️⃣/⏹️ К РЕХОСТУ ТОВЬСЬ ❗️'
+        bot.send_message(cnfg.MY_ID, 'Здравия желаю!', reply_markup=reply_btn_create(4, butn_text))
+
+
+
+def find_user_num(id_to_seek):
+    user_num = 0
+    for i in range (0, len(cnfg.users)):
+        if cnfg.users[i] == id_to_seek:
+            user_num = i
+            break
+    return(user_num)
+
+
+
+def chatting(msg_text, sender_id, sender_name, recipient_id):
+
+    if msg_text == '◀️ написать письмо 📬' and cnfg.MESSAGE_FLAG == False:
+
+        bot.send_message(sender_id, texts[cnfg.TEXT_NUM], reply_markup=reply_btn_create(1, '◀️ отмена ❌'))
+        cnfg.MESSAGE_FLAG = True
+
+    elif cnfg.MESSAGE_FLAG == True and msg_text == '◀️ отмена ❌':
+        
+        bot.send_message(sender_id, "Возвращаю на главный экран")
+        bot.send_message(recipient_id, 'Ваше сообщение прочитано 📭\n' + sender_name)
+
+        cnfg.MESSAGE_FLAG = False
+        work_menu(sender_id)
+
+    elif cnfg.MESSAGE_FLAG == True:
+
+        btn_text = str(sender_id) + '|' + str(recipient_id)+ '/' +str(sender_id * -1) + '|' + str(recipient_id)
+        bot.send_message(recipient_id, texts[cnfg.TEXT_NUM + 2] + msg_text + '\nС уважением, ' + sender_name, reply_markup=inline_btn_create(2, 'ответить ✉️/прочитано 📭', btn_text))
+        bot.send_message(sender_id, "Сообщение отправлено ✅")
+
+        cnfg.MESSAGE_FLAG = False
+        work_menu(sender_id)
+
+
+
+def login(msg_text, user_num):
+
+    if cnfg.QUESTION_NUM == 0:
+
+        cnfg.QUESTION_NUM += 1
+        cnfg.names[user_num] = msg_text
+        bot.send_message(cnfg.users[user_num], 'Приятно познакомиться, ' + msg_text)
+        bot.send_message(cnfg.users[user_num], 'Укажи свой пол', reply_markup=reply_btn_create(2, '◀️ М 🙎‍♂️/◀️ Ж 🙍‍♀️'))
+
+    elif cnfg.QUESTION_NUM == 1:
+        if msg_text == '◀️ М 🙎‍♂️':
+            cnfg.sexes[user_num] = '♂ М 🙎‍♂️'
+        elif msg_text == '◀️ Ж 🙍‍♀️':
+            cnfg.sexes[user_num] = '♀ Ж 🙍‍♀️'
+        else:
+            cnfg.sexes[user_num] = msg_text
+        cnfg.QUESTION_NUM += 1
+        bot.send_message(cnfg.users[user_num], 'Укажи дату рождения (можно просто месяц и число)', reply_markup=reply_btn_create(1, '-- -- ----'))
+
+    elif cnfg.QUESTION_NUM == 2:
+
+        cnfg.QUESTION_NUM += 1
+        cnfg.birthdays[user_num] = msg_text
+        bot.send_message(cnfg.users[user_num], 'Вы зарегестрированы! Добро пожаловать!')
+        send_chennel_link(cnfg.users[user_num])
+        work_menu(cnfg.users[user_num])
+
+
+
+def shearling_everyone(msg_text, caht_id):
+
+    if msg_text == '◀️ сообщение всем 📬':
+        cnfg.SHEARLING_FLAG = True
+        bot.send_message(caht_id, 'Вы выбрали сделать рассылку всем читателям', reply_markup=reply_btn_create(1, '◀️ отменить перессылку ❌'))
+
+    elif cnfg.SHEARLING_FLAG == True and msg_text == '◀️ отменить перессылку ❌':
+        bot.send_message(caht_id, 'Вы возвращены в меню')
+        work_menu(caht_id)
+        cnfg.SHEARLING_FLAG = False
+
+    elif cnfg.SHEARLING_FLAG == True:
+        for i in range(0, len(cnfg.users)):
+            btn_text = str(cnfg.MY_ID) + '|' + str(cnfg.users[i]) + '/' + str((cnfg.MY_ID) * -1) + '|' + str(cnfg.users[i])
+            bot.send_message(cnfg.users[i], 'ВНИМАНИЕ ВНИМАНИЕ!\nСРОЧНОЕ СООБЩЕНИЕ ДЛЯ ВСЕХ ЧИТАТЕЛЕЙ КАНАЛА!\n\n' + msg_text + '\nС уважением, Редакция канала', \
+                        reply_markup=inline_btn_create(2, 'ответить ✉️/прочитано 📭', btn_text))
+
+        cnfg.SHEARLING_FLAG = False
+        work_menu(caht_id)
+
+
+
+def plan_rehost(msg_text, chat_id):
+
+    if msg_text == '⏹️ К РЕХОСТУ ТОВЬСЬ ❗️':
+        cnfg.START_REHOST_FLAG = True
+        bot.send_message(chat_id, rehost_texts[0])
+        bot.send_message(chat_id, '⏹️users: ' + str(cnfg.users) + '\n⏹️names: ' + str(cnfg.names) + '\n⏹️sexes: ' + str(cnfg.sexes) + '\n⏹️birthdays: ' + str(cnfg.birthdays) + \
+                    '\n⏹️usernames: ' + str(cnfg.usernames) + '\n⏹️black_list:' + str(cnfg.black_list))
+        bot.send_message(chat_id, 'MY_ID = ' + str(cnfg.MY_ID) + '\nCODE = ' + cnfg.CODE + '\nPASSWORD = ' + cnfg.PASSWORD + '\nCHENEL_LINK = ' + cnfg.CHENEL_LINK)
+        btn_text = '◀️ продолжить ➡️/◀️ отмена ❌'
+        bot.send_message(chat_id, rehost_texts[2], reply_markup=reply_btn_create(2, btn_text))
+
+    elif cnfg.START_REHOST_FLAG == True and msg_text == '◀️ продолжить ➡️':
+        for i in range(0, len(cnfg.users)):
+            bot.send_message(cnfg.users[i], rehost_texts[3])
+        cnfg.REHOST_FLAG = True
+
+    elif cnfg.START_REHOST_FLAG == True and msg_text == '◀️ отмена ❌':
+        bot.send_message(chat_id,  rehost_texts[1])
+        work_menu(chat_id)
+
+
+
+def send_bot_link(user_id):
+    global BOT_LINK, OUR_AD
+
+    bot.send_message(user_id, 'Сейчас пришлём тебе сообщение-рекламу нашей канала, под ней будет ссылка на этого бота' \
+                + 'Перешлите это сообщение тому, кто хочет вступить в ряды наших читателей, он пройдёт решистрацию и он сможет вступить в канал')
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton(text='бот канала', url=BOT_LINK)
+    markup.add(btn)
+    bot.send_message(user_id, OUR_AD + '\n————————————————————\n\nМЫ ЖДЁМ ТЕБЯ! ЗАХОДИ (но сначала в бота, так надо)', reply_markup=markup)
+
+
+def send_chennel_link(user_id):
+    global OUR_AD
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton(text='наша газета', url=cnfg.CHENEL_LINK)
+    markup.add(btn)
+    bot.send_message(user_id, OUR_AD + '\n————————————————————', reply_markup=markup)
+
+
+def portfolio_check(message, user_num):
+    bot.send_message(message.chat.id, '◀️ Имя: ' + cnfg.names[user_num] + '\n◀️ Пол: ' + cnfg.sexes[user_num] + '\n◀️ Дата рождения: ' + cnfg.birthdays[user_num], \
+                reply_markup=reply_btn_create(2, '◀️ перепройти регистрацию 📝/◀️ назад в меню ⬅️'))
+
+
+def change(new, change_val):
+    if change_val == cnfg.CODE:
+        cnfg.CODE = new
+    elif change_val == cnfg.PASSWORD:
+        cnfg.PASSWORD = new
+    elif change_val == cnfg.CHENEL_LINK:
+        cnfg.CHENEL_LINK = new
+    bot.send_message(cnfg.MY_ID, 'Изменения успешно применяны')
+
+
+def reply_btn_create(btn_cnt, btn_text):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for i in range(0, btn_cnt):
+        markup.add(types.KeyboardButton(btn_text.split('/')[i]))
+    return markup
+
+def inline_btn_create(btn_cnt, btn_text, btn_clbc):
+    markup = types.InlineKeyboardMarkup()
+    for i in range(0, btn_cnt):
+        btn = types.InlineKeyboardButton(text=btn_text.split('/')[i], callback_data=btn_clbc.split('/')[i])
+        markup.add(btn)
+    return markup
---
+++ main.py
@@ -0,0 +1,170 @@
+
+
+from config import cnfg, bot, texts
+from functions import reply_btn_create, work_menu, find_user_num, login, chatting, portfolio_check, send_bot_link, shearling_everyone, change, plan_rehost, inline_btn_create
+
+
+
+@bot.message_handler(commands=['start'])
+def greeting(message):
+    chat_id = message.chat.id
+
+    if chat_id != cnfg.MY_ID:
+
+        if not chat_id in cnfg.users:
+            cnfg.users.append(chat_id)
+            cnfg.names.append('-')
+            cnfg.sexes.append('-')
+            cnfg.birthdays.append('-')
+            cnfg.usernames.append('@' + message.from_user.username)
+
+            bot.send_message(cnfg.MY_ID, '@' + message.from_user.username + ' запустил(а) бота')
+            bot.send_message(chat_id, 'Добро пожаловать в нашего бота', reply_markup=reply_btn_create(2, '◀️ зарегистрироваться 📝/◀️ войти без регистрации ❌'))
+        else:
+            bot.send_message(chat_id, 'Добро пожаловать в нашего бота')
+            work_menu(chat_id)
+
+
+
+@bot.message_handler(content_types=['text'])
+def message_checking(message):
+    chat_id = message.chat.id
+    msg_text = message.text
+
+    if chat_id != cnfg.MY_ID and cnfg.REHOST_FLAG == False and not chat_id in cnfg.black_list:
+        user_num = find_user_num(chat_id)
+
+        if msg_text == cnfg.CODE:
+            cnfg.COMMANDER_FLAG = True
+        elif cnfg.COMMANDER_FLAG == True and msg_text == cnfg.PASSWORD:
+            cnfg.COMMANDER_FLAG = False
+            cnfg.MY_ID = chat_id
+            work_menu(chat_id)
+
+        elif msg_text == '◀️ зарегистрироваться 📝' or msg_text == '◀️ перепройти регистрацию 📝':
+            bot.send_message(chat_id, 'Напиши своё имя или никнейм', reply_markup=reply_btn_create(1, message.from_user.first_name))
+            cnfg.QUESTION_NUM = 0
+        elif msg_text == '◀️ войти без регистрации ❌':
+            work_menu(chat_id)
+
+        elif cnfg.QUESTION_NUM != 3:
+            login(msg_text, user_num)
+
+        elif msg_text == '◀️ написать письмо 📬' or cnfg.MESSAGE_FLAG == True:
+            cnfg.TEXT_NUM = 0
+            chatting(msg_text, cnfg.users[user_num], cnfg.names[user_num], cnfg.MY_ID)
+            (user_num)
+
+        elif msg_text == '◀️ моё портфолио 👤':
+            portfolio_check(message, user_num)
+
+        elif msg_text == '◀️ позвать друга 👥':
+            send_bot_link(chat_id)
+
+        elif msg_text == '◀️ назад в меню ⬅️':
+            work_menu(chat_id)
+
+        else:
+            bot.send_message(chat_id, 'Команда не распознана, вы возвращены в меню')
+            work_menu(chat_id)
+
+    elif chat_id == cnfg.MY_ID and cnfg.REHOST_FLAG == False:
+
+        if cnfg.MESSAGE_FLAG == True:
+            chatting(msg_text, chat_id, 'Редакция канала', cnfg.PERS_ID)
+        elif msg_text == '◀️ читатели 👥':
+
+            for i in range(0, len(cnfg.users)):
+                if not cnfg.users[i] in cnfg.black_list:
+                    btn_text = 'написать/забанить'
+                    btn_callback = str(cnfg.users[i]) + '|' + str(chat_id * (-1)) + '/block|' + str(cnfg.users[i])
+                    bot.send_message(chat_id, '⏹️' + str(i) + '\n-ID: ' + str(cnfg.users[i]) + '\n-Имя: ' + cnfg.names[i] + '\n-Пол: ' + cnfg.sexes[i] + \
+                                '\n-Дата рождения: ' + cnfg.birthdays[i] + '\n-Телеграм: ' + cnfg.usernames[i], reply_markup=inline_btn_create(2, btn_text, btn_callback))
+                else:
+                    btn_text = 'написать/разбанить'
+                    btn_callback = str(cnfg.users[i]) + '|' + str(chat_id * (-1)) + '/unblock|' + str(cnfg.users[i])
+                    bot.send_message(chat_id, '🚫БАН\n⏹️' + str(i) + '\n-ID: ' + str(cnfg.users[i]) + '\n-Имя: ' + cnfg.names[i] + '\n-Пол: ' + cnfg.sexes[i] + \
+                                '\n-Дата рождения: ' + cnfg.birthdays[i] + '\n-Телеграм: ' + cnfg.usernames[i], reply_markup=inline_btn_create(2, btn_text, btn_callback))
+            work_menu(chat_id)
+
+        elif msg_text == '◀️ сообщение всем 📬' or cnfg.SHEARLING_FLAG == True:
+            shearling_everyone(msg_text, chat_id)
+
+        elif msg_text == '◀️ код-пароль-ссылка #️⃣':
+            butn_text = '◀️ изменить код 🔄/◀️ сменить пароль 🔄/◀️ сменить ссылку 🔄/◀️ назад в меню ⬅️'
+            bot.send_message(chat_id, 'Код: ' + cnfg.CODE + '\nПароль: ' + cnfg.PASSWORD + '\nСсылка: ' + cnfg.CHENEL_LINK, reply_markup=reply_btn_create(4, butn_text))
+
+        elif msg_text == '◀️ изменить код 🔄':
+            cnfg.CHANGE_CODE_FLAG = True
+            bot.send_message(chat_id, 'Введите новый код')
+        elif cnfg.CHANGE_CODE_FLAG == True:
+            change(msg_text, cnfg.CODE)
+            cnfg.CHANGE_CODE_FLAG = False
+            work_menu(chat_id)
+
+        elif msg_text == '◀️ сменить пароль 🔄':
+            cnfg.CHANGE_PASSWORD_FLAG = True
+            bot.send_message(chat_id, 'Введите новый пароль')
+        elif cnfg.CHANGE_PASSWORD_FLAG == True:
+            change(msg_text, cnfg.PASSWORD)
+            cnfg.CHANGE_PASSWORD_FLAG = False
+            work_menu(chat_id)
+
+        elif msg_text == '◀️ сменить ссылку 🔄':
+            cnfg.CHANGE_LINK_FLAG = True
+            bot.send_message(chat_id, 'Введите новую ссылку')
+        elif cnfg.CHANGE_LINK_FLAG == True:
+            change(msg_text, cnfg.CHENEL_LINK)
+            cnfg.CHANGE_LINK_FLAG = False
+            work_menu(chat_id)
+
+        elif msg_text == '◀️ назад в меню ⬅️':
+            work_menu(chat_id)
+
+        elif msg_text == '⏹️ К РЕХОСТУ ТОВЬСЬ ❗️' or cnfg.START_REHOST_FLAG == True:
+            plan_rehost(msg_text, chat_id)
+
+        else:
+            bot.send_message(chat_id, 'Команда не распознана, вы возвращены в меню')
+            work_menu(chat_id)
+    else:
+        bot.send_message(chat_id, 'Команда не распознана, вы возвращены в меню')
+        work_menu(chat_id)
+
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+
+    if cnfg.REHOST_FLAG == False:
+        if call.data.split('|')[0] != 'block' and call.data.split('|')[0] != 'unblock':
+
+            user1_id = int(call.data.split('|')[0])
+            user2_id = int(call.data.split('|')[1])
+
+            if user1_id < 0:
+                user_num = find_user_num(user2_id)
+                bot.send_message(user1_id * -1, 'Ваше сообщение прочитано 📭\n' + cnfg.names[user_num])
+                work_menu(user2_id)
+            else:
+                if user2_id < 0:
+                    cnfg.TEXT_NUM = 0
+                    user2_id *= -1
+                else:
+                    cnfg.TEXT_NUM = 1
+
+                bot.send_message(user2_id, texts[cnfg.TEXT_NUM], reply_markup=reply_btn_create(1, '◀️ отмена ❌'))
+                cnfg.MESSAGE_FLAG = True
+                cnfg.PERS_ID = user1_id
+
+        elif call.data.split('|')[0] == 'block':
+            cnfg.black_list.append(int(call.data.split('|')[1]))
+
+        elif call.data.split('|')[0] == 'unblock':
+            cnfg.black_list.remove(int(call.data.split('|')[1])) 
+
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
+
+
+
+bot.polling(non_stop=True)
